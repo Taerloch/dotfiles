3536ce82588dd7b2a2ca7151d8e417e02c0fd04c
chuckrichmond <chuck@firetoad.net>
2021-11-26 10:07:34 -0800

    adding in vim install

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ cp ./*.config $DOTFILE_DIR
 ln -f $DOTFILE_DIR/zshrc.config ~/.zshrc
 ln -f $DOTFILE_DIR/tmux.config ~/.tmux.conf
 ln -f $DOTFILE_DIR/alias.config ~/.zsh_alias``
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
