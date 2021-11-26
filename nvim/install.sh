50d57782bfe72039b2fc6173ac4d83474aeede9d
chuckrichmond <chuck@firetoad.net>
2021-11-26 12:18:53 -0800

    adding in vim install

--- nvim/install.sh
+++
@@ -1,2 +0,0 @@
-wget https://github.com/neovim/neovim/releases/download/v0.5.1/nvim.appimage
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
