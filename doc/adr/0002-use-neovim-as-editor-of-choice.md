e7a8b9ae6449b2044723c757532f15d179738644
Chuck Richmond <Chuck.Richmond@paccar.com>
2022-03-07 16:01:52 -0800

    vimandantigen (#3)
    * adding my vim folders
    
    * adding adr
    
    * adding .vimrc
    
    * adding .vimrc
    
    * adding in vim install
    
    * adding in vim install
    
    * adding in vim install
    
    * adding in vim install
    
    * adding in vim install
    
    * adding in vim install
    
    * :
    
    * adding updated .zshrc
    
    * adding in vim install
    
    * adding in vim install
    
    * this is the life
    
    * adding some things based on vim videos
    
    * adding some things based on vim videos
    
    * dotbot added
    
    * dotbot added
    
    * bricks are bd mkay
    
    * fixed zshrc
    
    * fixed git ignore
    
    * fixed git ignore
    
    * fixed nvim and plugs
    
    * fixed nvim and plugs
    
    * fixed nvim and plugs

---
+++ doc/adr/0002-use-neovim-as-editor-of-choice.md
@@ -0,0 +1,19 @@
+# 2. Use NeoVim as Editor of Choice
+
+Date: 2021-11-22
+
+## Status
+
+Accepted
+
+## Context
+
+I want to stay in the Command line as much as possible when working with CLI and automation scripting.
+
+## Decision
+
+I will use NeoVim as my primary editor in Linux Command Lines.
+
+## Consequences
+
+I am abandoning any wish to ever use EMACS. My reflexive windows copy paste is slowed down.
--- install.sh
+++ install.sh
@@ -1,8 +1,20 @@
+#! sh
 DOTFILE_DIR=~/.config/dotfiles
+ZSH=	
 echo $DOTFILE_DIR
 mkdir -p $DOTFILE_DIR 
 cp ./*.config $DOTFILE_DIR
+cp -r init.vim $DOTFILE_DIR
+#
+sh -c "$(curl -L https://github.com/zsh-users/antigen/releases/download/v2.2.3/antigen.zsh > antigen.zsh)"
+mv ./antigen.zsh $DOTFILE_DIR
+ln -fs $DOTFILE_DIR/antigen.zsh ~/.local/bin
 
+# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# cp z.sh ~/z.sh
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -f $DOTFILE_DIR/zshrc.config ~/.zshrc
 ln -f $DOTFILE_DIR/tmux.config ~/.tmux.conf
-ln -f $DOTFILE_DIR/alias.config ~/.zsh_alias``
+ln -f $DOTFILE_DIR/alias.config ~/.zsh_alias
+ln -f $DOTFILE_DIR/vimrc.config ~/.vimrc
+ln -fs $DOTFILE_DIR/init.vim ~/.config/nvim/init.vim
