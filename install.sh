1341ae07a18cc56a1a01b9b45cea828d7cdd15d5
Chuck Richmond <chuck.richmond@paccar.com>
2023-01-13 13:09:48 -0800

    fixing some pan issues

--- install.sh
+++ install.sh
@@ -8,13 +8,13 @@ cp -r init.vim $DOTFILE_DIR
 #
 sh -c "$(curl -L https://github.com/zsh-users/antigen/releases/download/v2.2.3/antigen.zsh > antigen.zsh)"
 mv ./antigen.zsh $DOTFILE_DIR
-ln -fs $DOTFILE_DIR/antigen.zsh ~/.local/bin
+ln -fs $DOTFILE_DIR/antigen.zsh ~/.local/bin/antigen.zsh
 
 # sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# cp z.sh ~/z.sh
+cp z.sh ~/z.sh
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -f $DOTFILE_DIR/zshrc.config ~/.zshrc
 ln -f $DOTFILE_DIR/tmux.config ~/.tmux.conf
 ln -f $DOTFILE_DIR/alias.config ~/.zsh_alias
 ln -f $DOTFILE_DIR/vimrc.config ~/.vimrc
-ln -fs $DOTFILE_DIR/init.vim ~/.config/nvim/init.vim
+#ln -fs $DOTFILE_DIR/init.vim ~/.config/nvim/init.vim
