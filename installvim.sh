6e77775b415fb282da8defa69a5729e9549fc085
Taerloch Richmond <tairloch@gmail.com>
2023-10-07 10:29:35 -0700

    tweaked the zsh

--- installvim.sh
+++ installvim.sh
@@ -1,3 +1,11 @@
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt-get update
-sudo apt-get install neovim
+wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+tar -zxf nvim-linux64.tar.gz
+mv nvim-linux64 /usr/share/nvim/bin/nvim
+CUSTOM_NVIM_PATH=/usr/share/nvim/bin/nvim
+# Set the above with the correct path, then run the rest of the commands:
+set -u
+sudo update-alternatives --install /usr/bin/ex ex /usr/share/nvim/bin/nvim 110
+sudo update-alternatives --install /usr/bin/vi vi  /usr/share/nvim/bin/nvim 110
+sudo update-alternatives --install /usr/bin/view view  /usr/share/nvim/bin/nvim 110
+sudo update-alternatives --install /usr/bin/vim vim /usr/share/nvim/bin/nvim 110
+sudo update-alternatives --install /usr/bin/vimdiff vimdiff /usr/share/nvim/bin/nvim 110  
